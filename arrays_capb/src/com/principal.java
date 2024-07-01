e91c4430ece4453a030cd235dfab3ae51b5f7137
CAPB11 <capardo.cer@gmail.com>
2024-07-01 09:21:29 -0600

    practica proyecto completo

---
+++ arrays_capb/src/com/principal.java
@@ -0,0 +1,91 @@
+/*
+ vamos a explorar indices en arrays
+ x.length(); --> metodo
+ prodpoiedad */
+
+
+
+package com;
+
+public class principal {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		 for (int i = 0; i < 10; i++) {
+	           for (int j = 0; j<=i;  j++)
+	               System.out.print("*");
+	           System.out.println();
+	       }}}
+	
+	/*
+	 String[][] a = {{"1", "2", "A", "3"}, {"4", "5", "6", "B"}, {"C", "7", "8", "9"}, {"D", "E", "F", "G"}};
+     String[][] b = {{"1", "X", "2", "3"}, {"Y", "4", "5", "Z"}, {"6", "7", "8", "9"}, {"A", "B", "C", "D"}};
+     int c [] [] = new int [4][4];
+
+  
+     for (int i = 0; i < a.length; i++) {
+         for (int j = 0; j < a[i].length; j++) {
+             try {
+                 int num1 = Integer.parseInt(a[i][j]);
+                 int num2 = Integer.parseInt(b[i][j]);
+                 c[i][j] = num1 + num2;
+             } catch (NumberFormatException e) {
+                 c[i][j] = 0; 
+                 // Set to 0 if any element is not a valid integer
+             }}}}}
+         
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String caracteres="b4e5vn59m0j98nb363¡()/#¡¡#$b5u4/(6v5y79Ñ%24v565823440¿204245JO/23t*&/M(O/c234";
+		String c="";
+		String nums ="";
+		for(int i=0; i<=caracteres.length()-1;i++) {
+			
+			c = Character.toString(caracteres.charAt(i));
+			
+		
+		
+					
+				}
+
+
+	*/
+	
+
+
